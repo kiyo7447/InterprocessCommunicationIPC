b4d3857d48f3f858cba25a1558e705b8cd5199cc
Kiyotaka Abe <kiyo7447@gmaill.com>
2023-09-18 15:07:00 +0900

    Clean up the code

--- NamedPipes/NamedPipeServer/Program.cs
+++ NamedPipes/NamedPipeServer/Program.cs
@@ -13,61 +13,10 @@ using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("testpipe"))
 
     Console.WriteLine("クライアントが接続しました。");
     using (System.IO.StreamWriter sw = new System.IO.StreamWriter(pipeServer))
-    // using (StreamReader sr = new StreamReader(pipeServer))
     {
-        sw.AutoFlush = true;
-        
+        sw.AutoFlush = true;        
         sw.WriteLine("Hello, Named Pipe");
-
-/*
-        while (true)
-        {
-            string messageFromClient = sr.ReadLine();
-            Console.WriteLine($"クライアントから受信: {messageFromClient}");
-
-            string response = $"サーバーからの応答: {messageFromClient}";
-            sw.WriteLine(response);
-
-            if (messageFromClient == "終了")
-            {
-                break;
-            }
-        }
- */    
     }
 }
 
 Console.WriteLine("サーバーが終了しました。");
-
-/*
-
-using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("testpipe"))
-{
-    Console.WriteLine("NamedPipeServer がクライアントの接続を待っています...");
-    pipeServer.WaitForConnection();
-
-    Console.WriteLine("クライアントが接続しました。");
-    using (StreamWriter sw = new StreamWriter(pipeServer))
-    using (StreamReader sr = new StreamReader(pipeServer))
-    {
-        sw.AutoFlush = true;
-
-        while (true)
-        {
-            string messageFromClient = sr.ReadLine();
-            Console.WriteLine($"クライアントから受信: {messageFromClient}");
-
-            string response = $"サーバーからの応答: {messageFromClient}";
-            sw.WriteLine(response);
-
-            if (messageFromClient == "終了")
-            {
-                break;
-            }
-        }
-    }
-}
-
-Console.WriteLine("サーバーが終了しました。");
-
- */
